c8ddf0299ec6d15262e1c8300e0fb79e1bf88b26
root <root@ip-172-31-88-109.ec2.internal>
2022-07-04 03:46:32 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# serverless_lambda_api
+# Corrected version of the LAB: Learn Terraform - Lambda functions and API Gateway
+
+Official Page:
+https://learn.hashicorp.com/tutorials/terraform/lambda-api-gateway?in=terraform/aws
+
---
+++ s3.tf
@@ -0,0 +1,33 @@
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
+
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "learn-terraform-functions"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "lambda_bucket"{
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl = "private"
+}
+
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
+
